5efda48b6bbb43fe8ec12f33a0f82f9b654bf0a4
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-01-17 13:14:28 +0100

    dunno

--- server.py
+++ server.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-print(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
+
+@app.route("/<username>")
+def hello_world(username=None):
+    return render_template('index.html', name=username)
+
+@app.route("/blog.html")
+def hello_world():
+    return '<p>It\'s blog about dogs!</p>'
