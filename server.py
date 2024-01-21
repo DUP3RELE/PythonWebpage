1e7a6602adb2662edd1caae3ec77d686e3c0055f
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-01-21 18:10:59 +0100

    modifinig link

--- server.py
+++ server.py
@@ -2,14 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
-
-@app.route("/<username>")
-def hello_world(username=None):
-    return render_template('index.html', name=username)
-
-@app.route("/blog.html")
-def hello_world():
-    return '<p>It\'s blog about dogs!</p>'
+@app.route("/<username>/<int:post_id>")
+def hello_world(username:None, post_id:None):
+    return render_template('index.html', name=username, post_id=post_id)
