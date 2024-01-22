4e35a4a6bf9b574566f10e4507a01c364cc660f7
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-01-22 15:09:14 +0100

    data saving

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
+import csv
 
 app = Flask(__name__)
 
@@ -6,26 +7,30 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route("/index.html")
-def home1():
-    return render_template('index.html')
+@app.route("/<string:page_name>")
+def htmlPage(page_name):
+    return render_template(page_name)
 
-@app.route("/about.html")
-def about():
-    return render_template('about.html')
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email},{subject},{message}')
 
-@app.route("/work.html")
-def work():
-    return render_template('work.html')
+def write_to_csv(data):
+    with open('database.csv', mode='a') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
 
-@app.route("/contact.html")
-def contact():
-    return render_template('contact.html')
-
-@app.route("/works.html")
-def works():
-    return render_template('works.html')
-
-@app.route("/components.html")
-def components():
-    return render_template('components.html')
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+   if request.method == 'POST':
+       data = request.form.to_dict()
+       write_to_csv(data)
+       return redirect('/thankyou.html')
+   else:
+       return 'something went wrong, try again'
